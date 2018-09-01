051f86a090a0812ccd8efa973467140572e54ce0
zero4drift <fang0052@e.ntu.edu.sg>
2018-09-01 15:45:01 +0800

    Add gooletest test cases.

--- part-1/chapter-2/insertion_sort.h
+++ part-1/chapter-2/insertion_sort.h
@@ -6,10 +6,20 @@
 
 template <typename T, std::size_t n> void insertion_sort(T (&array)[n])
 {
-  for(int j = 1; j != n; ++j)
+  /*
+   * for std::size_t is a alias of unsigned long,
+   * and the index in while loop must not be an unsigned,
+   * then index should be long long which could
+   * accommodate the maximum unsigned long integer
+   * to avoid bug.
+   */
+  if(n <= 1)
+    // in this case there is no need to sort.
+    return;
+  for(long long j = 1; j != n; ++j)
     {
       T temp = array[j];
-      int i = j - 1;
+      long long i = j - 1;
       while(i >=0 && array[i] > temp)
 	{
 	  array[i + 1] = array[i];
--- part-1/chapter-2/test_insertion_sort.cpp
+++ part-1/chapter-2/test_insertion_sort.cpp
@@ -1,13 +1,45 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include "insertion_sort.h"
 
-using std::cout;
-using std::endl;
-
-int main()
+TEST(InsertionSort, HandleNormalArray1)
 {
   int a[4] = {4, 3, 2, 1};
   insertion_sort(a);
-  for(int i = 0; i != 4; ++i)
-    cout << a[i] << endl;
+  EXPECT_EQ(1, a[0]);
+  EXPECT_EQ(2, a[1]);
+  EXPECT_EQ(3, a[2]);
+  EXPECT_EQ(4, a[3]);
+}
+
+TEST(InsertionSort, HandleNormalArray2)
+{
+  int a[4] = {1, 2, 3, 4};
+  insertion_sort(a);
+  EXPECT_EQ(1, a[0]);
+  EXPECT_EQ(2, a[1]);
+  EXPECT_EQ(3, a[2]);
+  EXPECT_EQ(4, a[3]);
+}
+
+TEST(InsertionSort, HandleNormalArray3)
+{
+  int a[4] = {2, 4, 3, 1};
+  insertion_sort(a);
+  EXPECT_EQ(1, a[0]);
+  EXPECT_EQ(2, a[1]);
+  EXPECT_EQ(3, a[2]);
+  EXPECT_EQ(4, a[3]);
+}
+
+TEST(InsertionSort, HandleOneElementArray)
+{
+  int a[1] = {4};
+  insertion_sort(a);
+  EXPECT_EQ(4, a[0]);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
