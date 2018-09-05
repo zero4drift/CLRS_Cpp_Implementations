d4e5d16984a3738b8a9857cf7f42c3d6d58cb671
zero4drift <fang0052@e.ntu.edu.sg>
2018-09-05 22:02:31 +0800

    Make variable naming style better.

--- part-1/chapter-4/square_matrix_multiply/square_matrix_multiply_recursive.h
+++ part-1/chapter-4/square_matrix_multiply/square_matrix_multiply_recursive.h
@@ -9,8 +9,8 @@ template <typename T, std::size_t n>
 void divide_matrix_4(T (&a)[n][n],
 		     T (&b)[n/2][n/2],
 		     T (&c)[n/2][n/2],
-		     T (&d)[n/2][n/2],
-		     T (&e)[n/2][n/2])
+		     T (&a00)[n/2][n/2],
+		     T (&a01)[n/2][n/2])
 {
   for(std::size_t i = 0; i != n / 2; ++i)
     for(std::size_t j = 0; j != n / 2; ++j)
@@ -20,26 +20,26 @@ void divide_matrix_4(T (&a)[n][n],
       c[i][j - n/2] = a[i][j];
   for(std::size_t i = n / 2; i != n; ++i)
     for(std::size_t j = 0; j != n / 2; ++j)
-      d[i - n/2][j] = a[i][j];
+      a00[i - n/2][j] = a[i][j];
   for(std::size_t i = n / 2; i != n; ++i)
     for(std::size_t j = n / 2; j != n; ++j)
-      e[i - n/2][j - n/2] = a[i][j];
+      a01[i - n/2][j - n/2] = a[i][j];
 }
 
 template <typename T, std::size_t n>
 void combine_matrix_4(T (&a)[n][n],
 		      T (&b)[n/2][n/2],
 		      T (&c)[n/2][n/2],
-		      T (&d)[n/2][n/2],
-		      T (&e)[n/2][n/2])
+		      T (&a00)[n/2][n/2],
+		      T (&a01)[n/2][n/2])
 {
   for(std::size_t i = 0; i != n / 2; ++i)
     for(std::size_t j = 0; j != n / 2; ++j)
       {
 	a[i][j] = b[i][j];
 	a[i][j + n/2] = c[i][j];
-	a[i + n/2][j] = d[i][j];
-	a[i + n/2][j + n/2] = e[i][j];
+	a[i + n/2][j] = a00[i][j];
+	a[i + n/2][j + n/2] = a01[i][j];
       }
 }
 
@@ -61,35 +61,35 @@ void square_matrix_multiply_recursive(T (&a)[1][1], T (&b)[1][1], T (&c)[1][1])
 template <typename T, std::size_t n>
 void square_matrix_multiply_recursive(T (&a)[n][n], T (&b)[n][n], T (&c)[n][n]) 
 {
-  T d[n/2][n/2];
-  T e[n/2][n/2];
-  T f[n/2][n/2];
-  T g[n/2][n/2];
-  divide_matrix_4(a, d, e, f, g);
-  T h[n/2][n/2];
-  T i[n/2][n/2];
-  T j[n/2][n/2];
-  T k[n/2][n/2];
-  divide_matrix_4(b, h, i, j, k);
-  T c1[n/2][n/2];
-  T c2[n/2][n/2];
-  square_matrix_multiply_recursive(d, h, c1);
-  square_matrix_multiply_recursive(e, j, c2);
-  T c3[n/2][n/2];
-  add_matrix(c3, c1, c2);
-  square_matrix_multiply_recursive(d, i, c1);
-  square_matrix_multiply_recursive(e, k, c2);
-  T c4[n/2][n/2];
-  add_matrix(c4, c1, c2);
-  square_matrix_multiply_recursive(f, h, c1);
-  square_matrix_multiply_recursive(g, j, c2);
-  T c5[n/2][n/2];
-  add_matrix(c5, c1, c2);
-  square_matrix_multiply_recursive(f, i, c1);
-  square_matrix_multiply_recursive(g, k, c2);
-  T c6[n/2][n/2];
-  add_matrix(c6, c1, c2);
-  combine_matrix_4(c, c3, c4, c5, c6);
+  T a00[n/2][n/2];
+  T a01[n/2][n/2];
+  T a10[n/2][n/2];
+  T a11[n/2][n/2];
+  divide_matrix_4(a, a00, a01, a10, a11);
+  T b00[n/2][n/2];
+  T b01[n/2][n/2];
+  T b10[n/2][n/2];
+  T b11[n/2][n/2];
+  divide_matrix_4(b, b00, b01, b10, b11);
+  T temp1[n/2][n/2];
+  T temp2[n/2][n/2];
+  square_matrix_multiply_recursive(a00, b00, temp1);
+  square_matrix_multiply_recursive(a01, b10, temp2);
+  T c00[n/2][n/2];
+  add_matrix(c00, temp1, temp2);
+  square_matrix_multiply_recursive(a00, b01, temp1);
+  square_matrix_multiply_recursive(a01, b11, temp2);
+  T c01[n/2][n/2];
+  add_matrix(c01, temp1, temp2);
+  square_matrix_multiply_recursive(a10, b00, temp1);
+  square_matrix_multiply_recursive(a11, b10, temp2);
+  T c10[n/2][n/2];
+  add_matrix(c10, temp1, temp2);
+  square_matrix_multiply_recursive(a10, b01, temp1);
+  square_matrix_multiply_recursive(a11, b11, temp2);
+  T c11[n/2][n/2];
+  add_matrix(c11, temp1, temp2);
+  combine_matrix_4(c, c00, c01, c10, c11);
 }
 
 
