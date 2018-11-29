802965cbfe1e68050e67b797006a3fab10da8651
zero4drift <fang0052@e.ntu.edu.sg>
2018-11-29 23:27:51 +0800

    Implement all pairs shortest paths: johnson algorithm.

--- test/all_pairs_shortest_paths_unittest.cpp
+++ test/all_pairs_shortest_paths_unittest.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include "chapter-24/single_source_shortest_paths/sssp_utilities.h"
 #include "chapter-25/all_pairs_shortest_paths/all_pairs_shortest_paths_utilities.h"
 #include "chapter-25/all_pairs_shortest_paths/matrix_all_pairs_shortest_paths/matrix_all_pairs_shortest_paths.h"
 #include "chapter-25/all_pairs_shortest_paths/floyd_warshall/floyd_warshall.h"
+#include "chapter-25/all_pairs_shortest_paths/johnson/johnson.h"
 
 using namespace CLRS;
 
@@ -87,4 +89,34 @@ TEST(AllPairsShortestPaths, TransitiveClosure)
   for(std::size_t i = 1; i != 4; ++i)
     for(std::size_t j = 0; j != 4; ++j)
       EXPECT_TRUE(t[i][j]);
+}
+
+TEST(AllPairsShortestPaths, Johnson)
+{
+  auto v0 = SSSPVertexGraph(0);
+  auto v1 = SSSPVertexGraph(1);
+  auto v2 = SSSPVertexGraph(2);
+  auto v3 = SSSPVertexGraph(3);
+  auto v4 = SSSPVertexGraph(4);
+  auto e0 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v0, v1, 3);
+  auto e1 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v2, v1, 4);
+  auto e2 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v0, v2, 8);
+  auto e3 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v1, v4, 7);
+  auto e4 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v1, v3, 1);
+  auto e5 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v3, v0, 2);
+  auto e6 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v0, v4, -4);
+  auto e7 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v4, v3, 6);
+  auto e8 = SSSPWeightEdgeGraph<SSSPVertexGraph>(v3, v2, -5);
+  std::vector<SSSPVertexGraph> vs = {v0, v1, v2, v3, v4};
+  std::vector<SSSPWeightEdgeGraph<SSSPVertexGraph>> es =
+    {e0, e1, e2, e3, e4, e5, e6, e7, e8};
+  LinkedListGraph<SSSPVertexGraph,
+		  SSSPWeightEdgeGraph<SSSPVertexGraph>> g(vs, es);
+  auto d = johnson(g);
+  EXPECT_EQ(0, d[0][0]);
+  EXPECT_EQ(1, d[0][1]);
+  EXPECT_EQ(1, d[1][3]);
+  EXPECT_EQ(3, d[2][4]);
+  EXPECT_EQ(-5, d[3][2]);
+  EXPECT_EQ(6, d[4][3]);
 }
